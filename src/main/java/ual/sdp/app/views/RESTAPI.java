d31f100011bbf74f9efdb579ae29376e85dc368b
mogilev <58267935+mogilev@users.noreply.github.com>
2021-01-11 02:19:25 +0000

    adicionados métodos para items/*

--- src/main/java/ual/sdp/app/views/RESTAPI.java
+++ src/main/java/ual/sdp/app/views/RESTAPI.java
@@ -12,14 +12,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/api")
+@WebServlet("/items/*")
 public class RESTAPI extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("application/json");
+        resp.setContentType("application/json"); // TODO: 1/11/2021
 
-        Data data = new Data("code", 42.0);
+        Data data = new Data("doGet", 42.0);
+
+        JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        jsonBuilder.add("name", data.getName());
+        jsonBuilder.add("value", data.getValue());
+
+        JsonWriter jsonWriter = Json.createWriter(resp.getWriter());
+        jsonWriter.writeObject(jsonBuilder.build());
+        jsonWriter.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json"); // TODO
+
+        Data data = new Data("doPost", 42.0);
+
+        JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        jsonBuilder.add("name", data.getName());
+        jsonBuilder.add("value", data.getValue());
+
+        JsonWriter jsonWriter = Json.createWriter(resp.getWriter());
+        jsonWriter.writeObject(jsonBuilder.build());
+        jsonWriter.close();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");  // TODO
+
+        Data data = new Data("doPut", 42.0);
+
+        JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
+        jsonBuilder.add("name", data.getName());
+        jsonBuilder.add("value", data.getValue());
+
+        JsonWriter jsonWriter = Json.createWriter(resp.getWriter());
+        jsonWriter.writeObject(jsonBuilder.build());
+        jsonWriter.close();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json"); // TODO
+
+        Data data = new Data("doDelete", 42.0);
 
         JsonObjectBuilder jsonBuilder = Json.createObjectBuilder();
         jsonBuilder.add("name", data.getName());
@@ -30,4 +75,3 @@ public class RESTAPI extends HttpServlet {
         jsonWriter.close();
     }
 }
-
